c0fdf4121f1b04d150290f3e6006f92fb55dfca9
berryxchange.innovations@gmail.com <berryxchange.innovations@gmail.com>
2022-07-26 21:13:39 -0500

    Updated Project with Travel App

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter basics',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        debugShowCheckedModeBanner: false,
+        title: "Hello World Travel App", //'Flutter basics',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TravelPage()
+        //Stage(name: "Flutter Basics"),
+        );
+  }
+}
+
+class TravelPage extends StatefulWidget {
+  const TravelPage({Key? key}) : super(key: key);
+
+  @override
+  State<TravelPage> createState() => _TravelPageState();
+}
+
+class _TravelPageState extends State<TravelPage> {
+  void contactUs({required BuildContext context}) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Contact Us"),
+            content: Text("Send E-mail to sucker@crap.com"),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    print("Sent Email");
+                  },
+                  child: Text("Close"))
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("Hello World Travel App"),
       ),
-      home: Stage(name: "Flutter Basics"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  "Hello World Travel",
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[800]),
+                ),
+                Text(
+                  "Discover the world",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple),
+                ),
+                Image.network(
+                  "https://cdn.mos.cms.futurecdn.net/PTmw2FetAK9jgNJ9TqLHFj-1200-80.jpg",
+                  height: 350,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: MaterialButton(
+                    onPressed: () {
+                      contactUs(context: context);
+                      print("Button was pressed!");
+                    },
+                    child: Text(
+                      "Enter",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                    ),
+                    color: Colors.blue[800],
+                    height: 75,
+                    minWidth: MediaQuery.of(context).size.width,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
